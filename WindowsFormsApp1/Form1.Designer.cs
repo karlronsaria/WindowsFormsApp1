124cc5bd065d4ec6dc4d342b4dfa66bfcfdcfe09
karlr <karlr@KASLOVT01>
2021-12-13 14:46:33 -0800

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openDirectoryStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pdfRenderer1 = new PdfiumViewer.PdfRenderer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openDirectoryStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(740, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openDirectoryStripMenuItem
+            // 
+            this.openDirectoryStripMenuItem.ForeColor = System.Drawing.SystemColors.Window;
+            this.openDirectoryStripMenuItem.Name = "openDirectoryStripMenuItem";
+            this.openDirectoryStripMenuItem.Size = new System.Drawing.Size(99, 20);
+            this.openDirectoryStripMenuItem.Text = "Open Directory";
+            this.openDirectoryStripMenuItem.Click += new System.EventHandler(this.openDirectoryStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(588, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(360, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Hi! This stupid white line ends here for some reason, and I can\'t get rid of it.";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pdfRenderer1);
+            this.splitContainer1.Size = new System.Drawing.Size(567, 350);
+            this.splitContainer1.SplitterDistance = 289;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.ForeColor = System.Drawing.SystemColors.Window;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(289, 350);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged_UsingItems);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Type";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Last Modified";
+            this.columnHeader3.Width = 87;
+            // 
+            // pdfRenderer1
+            // 
+            this.pdfRenderer1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pdfRenderer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pdfRenderer1.Location = new System.Drawing.Point(0, 0);
+            this.pdfRenderer1.Name = "pdfRenderer1";
+            this.pdfRenderer1.Page = 0;
+            this.pdfRenderer1.Rotation = PdfiumViewer.PdfRotation.Rotate0;
+            this.pdfRenderer1.Size = new System.Drawing.Size(274, 350);
+            this.pdfRenderer1.TabIndex = 2;
+            this.pdfRenderer1.Text = "pdfRenderer1";
+            this.pdfRenderer1.ZoomMode = PdfiumViewer.PdfViewerZoomMode.FitHeight;
+            // 
+            // treeView1
+            // 
+            this.treeView1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.ForeColor = System.Drawing.SystemColors.Window;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(145, 350);
+            this.treeView1.TabIndex = 6;
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            this.treeView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.treeView1_KeyDown);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer2.Location = new System.Drawing.Point(12, 27);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.splitContainer1);
+            this.splitContainer2.Size = new System.Drawing.Size(716, 350);
+            this.splitContainer2.SplitterDistance = 145;
+            this.splitContainer2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(740, 389);
+            this.Controls.Add(this.splitContainer2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openDirectoryStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private PdfiumViewer.PdfRenderer pdfRenderer1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using PdfiumViewer;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        const string STARTING_DIRECTORY = @"C:\Users\karlr\OneDrive\__POOL";
+        // const string STARTING_DIRECTORY = @"C:\";
+
+        private readonly PdfiumViewer.PdfViewer _pdf;
+        private IList<FileSystemInfo> _items;
+
+        public Form1()
+        {
+            InitializeComponent();
+            PopulateTreeView(STARTING_DIRECTORY);
+            _pdf = new PdfViewer();
+            this.Controls.Add(_pdf);
+        }
+
+        public void PreviewPdf(string filePath)
+        {
+            byte[] bytes = System.IO.File.ReadAllBytes(filePath);
+            pdfRenderer1.Load(PdfDocument.Load(new MemoryStream(bytes)));
+        }
+
+        private void openDirectoryStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                PopulateTreeView(dialog.SelectedPath);
+            }
+        }
+
+        private void PopulateTreeView(string directoryPath)
+        {
+            treeView1.Nodes.Clear();
+
+            TreeNode rootNode;
+            DirectoryInfo info = new DirectoryInfo(directoryPath);
+
+            if (info.Exists)
+            {
+                rootNode = new TreeNode
+                {
+                    Text = info.Name,
+                    Tag = info
+                };
+                
+                GetDirectories(info.GetDirectories(), rootNode);
+                treeView1.Nodes.Add(rootNode);
+            }
+        }
+
+        private void GetDirectories(DirectoryInfo[] subDirs, TreeNode nodeToAddTo)
+        {
+            TreeNode aNode;
+            DirectoryInfo[] subSubDirs;
+
+            foreach (DirectoryInfo subDir in subDirs)
+            {
+                try
+                {
+                    aNode = new TreeNode
+                    {
+                        Text = subDir.Name,
+                        ImageIndex = 0,
+                        SelectedImageIndex = 0,
+                        Tag = subDir,
+                        ImageKey = "folder"
+                    };
+
+                    subSubDirs = subDir.GetDirectories();
+
+                    if (subSubDirs.Length != 0)
+                    {
+                        GetDirectories(subSubDirs, aNode);
+                    }
+
+                    nodeToAddTo.Nodes.Add(aNode);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    continue;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // // No smaller than design time size
+            // this.MinimumSize = new System.Drawing.Size(this.Width, this.Height);
+
+            // No larger than screen size
+            this.MaximumSize = new System.Drawing.Size(
+                Screen.PrimaryScreen.Bounds.Width,
+                Screen.PrimaryScreen.Bounds.Height
+            );
+
+            // this.AutoSize = true;
+            // this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+        }
+
+        private void listView1_Load(TreeNode newSelected)
+        {
+            listView1.Items.Clear();
+            DirectoryInfo nodeDirInfo = (DirectoryInfo)newSelected.Tag;
+            ListViewItem.ListViewSubItem[] subItems;
+            ListViewItem item;
+
+            _items = new List<FileSystemInfo>();
+
+            foreach (DirectoryInfo dir in nodeDirInfo.GetDirectories())
+            {
+                try
+                {
+                    _items.Add(dir);
+                    item = new ListViewItem(dir.Name, 0);
+
+                    subItems = new ListViewItem.ListViewSubItem[] {
+                        new ListViewItem.ListViewSubItem(item, "Directory"),
+                        new ListViewItem.ListViewSubItem(
+                            item,
+                            dir.LastAccessTime.ToShortDateString()
+                        )
+                    };
+
+                    item.SubItems.AddRange(subItems);
+                    listView1.Items.Add(item);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    continue;
+                }
+            }
+
+            foreach (FileInfo file in nodeDirInfo.GetFiles()) 
+            {
+                try
+                {
+                    _items.Add(file);
+                    item = new ListViewItem(file.Name, 1);
+
+                    subItems = new ListViewItem.ListViewSubItem[] {
+                        new ListViewItem.ListViewSubItem(item, "File"),
+                        new ListViewItem.ListViewSubItem(
+                            item,
+                            file.LastAccessTime.ToShortDateString()
+                        )
+                    };
+
+                    item.SubItems.AddRange(subItems);
+                    listView1.Items.Add(item);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    continue;
+                }
+            }
+
+            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            listView1_Load(e.Node);
+        }
+
+        private void treeView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Enter:
+                case Keys.Space:
+                    if (treeView1.SelectedNode is TreeNode n)
+                    {
+                        listView1_Load(n);
+                    }
+
+                    // link: https://stackoverflow.com/questions/13952932/disable-beep-of-enter-and-escape-key-c-sharp
+                    // retrieved: 2021_12_13
+                    e.Handled = e.SuppressKeyPress = true;
+                    break;
+            }
+        }
+
+        private void listView1_SelectedIndexChanged_UsingItems(
+            object sender, System.EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                return;
+            }
+
+            var indices = listView1.SelectedIndices;
+            var lastIndex = indices[indices.Count - 1];
+            var viewItem = listView1.Items[lastIndex];
+            var modelItem = _items[lastIndex];
+
+            var fullName = modelItem.FullName;
+            var extension = modelItem.Extension;
+            var type = viewItem.SubItems[1].Text;
+
+            switch (type)
+            {
+                case "File":
+                    switch (extension)
+                    {
+                        case ".pdf":
+                            PreviewPdf(fullName);
+                            break;
+                    }
+
+                    break;
+            }
+        }
+    }
+}
+
+public class MySR : ToolStripSystemRenderer
+{
+    public MySR() { }
+
+    protected override void OnRenderToolStripBorder(ToolStripRenderEventArgs e)
+    {
+        base.OnRenderToolStripBorder(e);
+        e.Graphics.FillRectangle(Brushes.White, e.ConnectedArea);
+    }
+}
+

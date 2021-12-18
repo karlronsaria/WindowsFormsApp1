3275937872d6640b86c206c1aad97de1a6b32d65
whatwereyouthinking <karlronsaria@live.com>
2021-12-18 11:43:14 -0800

    fix(search box): ensure watermark text draws correctly

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -38,7 +38,7 @@
             this.splitContainer2 = new System.Windows.Forms.SplitContainer();
             this.splitContainer3 = new System.Windows.Forms.SplitContainer();
             this.tableLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.textBox1 = new external.WaterMarkTextBox();
+            this.textBox1 = new external.WatermarkTextBox();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
@@ -191,7 +191,7 @@
             this.textBox1.Size = new System.Drawing.Size(421, 20);
             this.textBox1.TabIndex = 7;
             this.textBox1.TextChanged += new System.EventHandler(textBox1_TextChanged);
-            this.textBox1.WaterMarkText = "Search";
+            this.textBox1.WatermarkText = "Search";
             // 
             // Form1
             // 
@@ -233,7 +233,7 @@
         private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.SplitContainer splitContainer2;
         private System.Windows.Forms.SplitContainer splitContainer3;
-        private external.WaterMarkTextBox textBox1;
+        private external.WatermarkTextBox textBox1;
         private System.Windows.Forms.FlowLayoutPanel tableLayoutPanel1;
     }
 }
--- WindowsFormsApp1/WatermarkTextBox.cs
+++ WindowsFormsApp1/WatermarkTextBox.cs
@@ -4,6 +4,112 @@ using System.Drawing;
 
 namespace external
 {
+    /// <link>
+    ///   <url>
+    ///   https://stackoverflow.com/questions/2487104/how-do-i-implement-a-textbox-that-displays-type-here/11575975
+    ///   https://stackoverflow.com/users/837726/joel
+    ///   </url>
+    ///   <retrievedata>
+    ///   2021_12_18
+    ///   </retrievedata>
+    /// </link>
+    /// <summary>
+    /// A textbox that supports a watermak hint.
+    /// </summary>
+    public class WatermarkTextBox : TextBox
+    {
+        /// <summary>
+        /// The text that will be presented as the watermak hint
+        /// </summary>
+        private string _watermarkText = "Type here";
+        /// <summary>
+        /// Gets or Sets the text that will be presented as the watermak hint
+        /// </summary>
+        public string WatermarkText
+        {
+            get { return _watermarkText; }
+            set { _watermarkText = value; }
+        }
+
+        /// <summary>
+        /// Whether watermark effect is enabled or not
+        /// </summary>
+        private bool _watermarkActive = true;
+        /// <summary>
+        /// Gets or Sets whether watermark effect is enabled or not
+        /// </summary>
+        public bool WatermarkActive
+        {
+            get { return _watermarkActive; }
+            set { _watermarkActive = value; }
+        }
+
+        /// <summary>
+        /// Create a new TextBox that supports watermak hint
+        /// </summary>
+        public WatermarkTextBox()
+        {
+            this._watermarkActive = true;
+            this.Text = _watermarkText;
+            this.ForeColor = Color.Gray;
+
+            GotFocus += (source, e) =>
+            {
+                RemoveWatermak();
+            };
+
+            LostFocus += (source, e) =>
+            {
+                ApplyWatermark();
+            };
+        }
+
+        /// <summary>
+        /// Remove watermark from the textbox
+        /// </summary>
+        public void RemoveWatermak()
+        {
+            if (this._watermarkActive)
+            {
+                this._watermarkActive = false;
+                this.Text = "";
+                this.ForeColor = Color.Black;
+            }
+        }
+
+        /// <summary>
+        /// Applywatermak immediately
+        /// </summary>
+        public void ApplyWatermark()
+        {
+            if (!this._watermarkActive
+                && string.IsNullOrEmpty(this.Text)
+                || ForeColor == Color.Gray)
+            {
+                this._watermarkActive = true;
+                this.Text = _watermarkText;
+                this.ForeColor = Color.Gray;
+            }
+        }
+
+        /// <summary>
+        /// Apply watermak to the textbox. 
+        /// </summary>
+        /// <param name="newText">Text to apply</param>
+        public void ApplyWatermark(string newText)
+        {
+            WatermarkText = newText;
+            ApplyWatermark();
+        }
+        
+        protected override void OnCreateControl() 
+        { 
+            base.OnCreateControl();
+            ApplyWatermark();
+        }
+    }
+
+    /*
     // link: https://www.codeproject.com/Articles/27849/WaterMark-TextBox-For-Desktop-Applications-Using-C
     // retrieved: 2021_12_18
     class WaterMarkTextBox : TextBox
@@ -16,8 +122,10 @@ namespace external
             public Color WaterMarkColor
             {
                 get { return _waterMarkColor; }
-                set { _waterMarkColor = value; Invalidate();/*thanks to Bernhard Elbl
-                                                              for Invalidate()*/ }
+                set {
+                    _waterMarkColor = value; Invalidate();
+                    // thanks to Bernhard Elbl for Invalidate()
+                }
             }
 
             private string _waterMarkText = "Water Mark";
@@ -116,4 +224,5 @@ namespace external
             }
         }
     }
+    */
 }
